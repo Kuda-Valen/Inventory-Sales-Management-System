31e4faee65fe87dfae7d3fa0c8126b44fe4899a4
Kuda_Valen <kudamukwasi@gmail.com>
2026-07-06 22:23:13 +0200

    m

--- models/product.py
+++ models/product.py
@@ -3,10 +3,10 @@ class Product():
     def __init__(self, name, cost_price, selling_price, stock, supplier_name):
         
         self.name = name
-        self.cost_price = 
-        self.selling_price = price
+        self.cost_price = cost_price
+        self.selling_price = selling_price
         self.stock = stock
-        self.supplier = supplier
+        self.supplier_name = supplier_name
     
     def update_stock(self):
         print("Update stock feature coming soon!")
