ce5aa415735fcdf26cbb939d9406bd3dd271f7ae
Kuda_Valen <kudamukwasi@gmail.com>
2026-07-11 22:48:56 +0200

    Adding products database

--- database/add_product_database.py
+++ database/add_product_database.py
@@ -12,5 +12,13 @@ CREATE TABLE IF NOT EXISTS products(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL, 
     cost_price REAL NOT NULL, 
-               )
+    selling_price REAL NOT NULL,
+    stock INTEGER NOT NULL,
+    supplier_name TEXT NOT NULL,
+    );
 """)
+
+conn.commit()
+conn.close()
+
+print("\nProducts Table has been created successfully...")
